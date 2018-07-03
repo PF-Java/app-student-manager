7d49e0714f427a8b7b986f9697bc88a3cf790a25
Quynhlethi <lequynh94.hrm.neu@gmail.com>
2018-07-03 21:14:40 +0700

    add test case to method displayAll()and method add, getInfomation

--- test/StudentManagerTest.java
+++ test/StudentManagerTest.java
@@ -3,10 +3,25 @@ import students.StudentManager;
 
 public class StudentManagerTest {
     public static void main(String[] args) {
-        Student trang = new Student("Trang", 23);
         StudentManager studentManager = new StudentManager();
+        studentManager.displayAll();
+
+
+        Student trang = new Student("Trang", 23);
         studentManager.add(trang);
         System.out.println("added " + trang.getInfomation());
         studentManager.displayAll();
+
+        Student loan = new Student("Loan", 25);
+        studentManager.add(loan);
+        System.out.println("added " + loan.getInfomation());
+        studentManager.displayAll();
+
+        Student anh = new Student("Anh", 20);
+        studentManager.add(anh);
+        System.out.println("added " + anh.getInfomation());
+        studentManager.displayAll();
+
+
     }
 }
