e5065ea3d5ef77815f389baedf975194661ab02c
Quynhlethi <lequynh94.hrm.neu@gmail.com>
2018-07-03 21:01:53 +0700

    pass test case object StudentManager and added object in list

--- src/students/StudentManager.java
+++ src/students/StudentManager.java
@@ -1,4 +1,13 @@
 package students;
 
 public class StudentManager {
+    private Student[] students = new Student[5];
+    private int size;
+    public StudentManager() {
+    }
+    public void add(Student student) {
+        students[size] = student;
+        size++;
+    }
+
 }
