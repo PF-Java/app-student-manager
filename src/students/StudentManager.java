309c971989011d333fb5ece00d818e19323907fc
Quynhlethi <lequynh94.hrm.neu@gmail.com>
2018-07-03 21:09:37 +0700

    pass test method displayAll()

--- src/students/StudentManager.java
+++ src/students/StudentManager.java
@@ -5,9 +5,26 @@ public class StudentManager {
     private int size;
     public StudentManager() {
     }
+
     public void add(Student student) {
         students[size] = student;
         size++;
+    }
+
+    public void displayAll() {
+        if (size == 0) {
+            System.out.println("There is no any student in ");
+        } else if (size == 1) {
+            System.out.println("There is 1 student in list");
+            System.out.print(students[0].getInfomation() + "");
+            System.out.println();
+        } else {
+            System.out.println("There are " + size + " student list:");
+            for (int i = 0; i < size; i++) {
+                System.out.print(students[i].getInfomation() + ", ");
+            }
+            System.out.println();
+        }
     }
 
 }
